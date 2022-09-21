36bdc022184115ebb391e237ef9b3762487eb4f4
Fafemi Adeola <adeola5678@gmail.com>
2022-09-21 11:15:46 +0100

    fix

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,10 +1,23 @@
 import React from "react"
+import axios from "axios"
 import useGet from "../../utils/useGet-hook"
 
 export default function Home() {
-  const { data, loading } = useGet(() => {
-    return "Hello"
-  }, [])
-  console.log(data)
-  return <div> {!loading && <p>Hello {data}!</p>}</div>
+  const [apiData, setApiData] = React.useState("")
+
+  const url = "api/gatsby-function"
+
+  const getData = () => {
+    const results = async () => {
+      try {
+        const response = await axios.post(url)
+        setApiData(response.data)
+      } catch (error) {}
+    }
+    results()
+    return apiData
+  }
+
+  const { data, loading } = useGet(getData, [])
+  return <div> {!loading && <p> {data}!</p>}</div>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,10 @@
 import React from "react"
-import axios from "axios"
 import useGet from "../../utils/useGet-hook"
 
 export default function Home() {
-  const [apiData, setApiData] = React.useState("")
-
-  const url = "api/gatsby-function"
-
-  const getData = () => {
-    const results = async () => {
-      try {
-        const response = await axios.post(url)
-        setApiData(response.data)
-      } catch (error) {}
-    }
-    results()
-    return apiData
-  }
-
-  const { data, loading } = useGet(getData, [])
-  return <div> {!loading && <p> {data}!</p>}</div>
+  const { data, loading } = useGet(() => {
+    return "Hello"
+  }, [])
+  console.log(data)
+  return <div> {!loading && <p>Hello {data}!</p>}</div>
 }
